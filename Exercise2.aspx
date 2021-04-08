58a13320dd7c0dfcb045e1350f222640826dc07c
Hsiang <htse856@aucklanduni.ac.nz>
2021-04-08 22:41:43 +1200

    Initial commit

---
+++ Exercise2.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Exercise2.aspx.cs" Inherits="Exercise2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Exercise2</title>
+    <style type="text/css"> 
+          #GridView2{
+              table-layout = fixed;
+              width:200px;
+          }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Select Employers:
+        <asp:DropDownList ID="DropDownList1" runat="server"  DataTextField="estab_name" DataValueField="estab_name" AutoPostBack="True" OnselectedIndexChanged="updateGridView" DataSourceID="SqlDataSource1">
+        </asp:DropDownList>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" 
+            SelectCommand="SELECT [estab_name] FROM [Employer_Master]">
+        </asp:SqlDataSource>
+    </div>
+        <br />
+        <br />
+        <asp:GridView ID="GridView2" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Exercise2.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+
+public partial class Exercise2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void updateGridView(object sender, EventArgs e)
+    {
+        try
+        {
+            StringBuilder sql = new StringBuilder();
+            sql.Append("SELECT b.[site_id] 'Site ID', b.[name] 'Site Name' ");
+            sql.Append("FROM [Employer_Master] a ");
+            sql.Append("JOIN [Site] b ON a.[group_estab_no] = b.[linked_estab_no] ");
+            sql.Append("WHERE  a.estab_name = '" + DropDownList1.SelectedValue + "' ");
+            sql.Append("AND a.[date_time_deleted] IS NULL AND b.[date_time_deleted] IS NULL");
+            SqlDataAdapter da = new SqlDataAdapter(sql.ToString(), System.Configuration.ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "site");
+            GridView2.DataSource = ds.Tables["site"].DefaultView;
+            GridView2.DataBind();
+        }
+        catch(Exception ex)
+        {
+            GridView2.Columns.Clear();
+            GridView2.DataBind();
+        }
+    }
+
+}
